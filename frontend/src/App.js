73a8ac8b19fe169b82dedfda2e55aab00b5b2193
ZuhaibShafi03 <zuhaib.shafi@ontariotechu.net>
2025-03-28 01:22:05 -0400

    Updated app.js to use Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Orders from './components/Orders';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/orders" element={<Orders />} />
-      </Routes>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
