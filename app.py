86743b785b77d9c06157911781bd095631929143
NavvSiva <abhinav.sivasakthi@ontariotechu.net>
2025-03-30 01:59:42 -0400

    order display issue

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from flask import Flask, request, jsonify
 import mysql.connector
 from functools import wraps
 from dotenv import load_dotenv
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 # Load environment variables from .env
 load_dotenv()
