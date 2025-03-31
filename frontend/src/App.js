b886d9a2846ace224c4b6510fd6a9e67ebb886d5
ZuhaibShafi03 <zuhaib.shafi@ontariotechu.net>
2025-03-31 18:48:11 -0400

    fixed stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Orders from './components/Orders';
-import Navbar from './components/Navbar';
-import Menu from './components/Menu';
-import StaffOrders from './components/StaffOrders';
-import Feedback from './components/Feedback';
-import Payment from './components/Payment';
-import About from './components/About';
+import Navbar from './Navbar';
+import About from './About';    
+import Menu from './Menu';      
+import Orders from './Orders';
+import Feedback from './Feedback';
+import Login from './Login';
+import Signup from './Signup';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Navbar />
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/staff-orders" element={<StaffOrders />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/about" element={<About />} />   
+        <Route path="/menu" element={<Menu />} />     
+        <Route path="/feedback" element={<Feedback />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
